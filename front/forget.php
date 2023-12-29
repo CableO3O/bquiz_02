4be29648dd1dbb1af03ddbabbca7a502b1342d91
cable <cable870526@gmail.com>
2023-12-29 13:09:06 +0800

    忘記密碼介面

--- front/forget.php
+++ front/forget.php
@@ -2,7 +2,7 @@
     <legend>忘記密碼</legend>
     <div>請輸入信箱以查詢密碼</div>
     <div>
-        <input type="text" name="acc" id="">
+        <input type="text" name="acc" id="acc">
     </div>
     <div id="result"></div>
     <div>
