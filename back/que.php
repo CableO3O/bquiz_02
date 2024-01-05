f9174809ab185fdba378e1f9c72170e870fec64d
cable <cable870526@gmail.com>
2024-01-05 14:25:09 +0800

    建立問卷管理

--- back/que.php
+++ back/que.php
@@ -10,7 +10,7 @@
         </div>
         <div>
             <div id="opt">
-                選項<input type="text" name="option">
+                選項<input type="text" name="option[]">
                 <input type="button" value="更多" onclick="more()">
             </div>
         </div>
@@ -23,7 +23,7 @@
 <script>
     function more() {
         let opt = `<div id="opt">
-            選項<input type="text" name="option">
+            選項<input type="text" name="option[]">
             </div>`
         $("#opt").before(opt);
     }
